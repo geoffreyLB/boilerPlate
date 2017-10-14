f14c04e0f2776b952c5f78fce32315014a466f5e
Geoffrey <geoffrey.leborges@epitech.eu>
2017-10-14 15:07:07 +0200

    babel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,2 @@
-const Koa = require('koa');
-const app = new Koa();
-
-app.use(async ctx => {
-  ctx.body = 'Hello old place';
-});
-
-app.listen(3000);
+require('babel-register');
+require('./src/server');
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
-# .PHONY: run
+#.PHONY: run
+
 run:
+	@echo "All apps started and running"
+	@echo "  API:          http://localhost:3000"
 	@./node_modules/.bin/concurrently "cd api && npm start"
